68ab6ac40ba0d27f653cee0faa698ceeab06649b
Alex <aleexcaso@gmail.com>
2022-10-31 14:39:57 +0100

    dfsadfsdf

--- XML/Workspace/XMLConverter/src/converter/XMLConverter.java
+++ XML/Workspace/XMLConverter/src/converter/XMLConverter.java
@@ -3,8 +3,8 @@ package converter;
 public class XMLConverter {
 
 	public static void main(String[] args) {
-		if(args.length < 2) {
-			throw new IllegalArgumentException("Se deben introducir al menos dos nombres");
+		if(args.length < 3) {
+			throw new IllegalArgumentException("Se deben introducir tres nombres");
 		}
 		String xmlName = args[0];
 		String output = args[1];
