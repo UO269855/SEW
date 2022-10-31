4274135a2ee2054dcfcc89b76ae51387795e7ed7
Alex <aleexcaso@gmail.com>
2022-10-31 14:37:22 +0100

    añadido java

---
+++ XML/Workspace/XMLConverter/src/converter/XMLConverter.java
@@ -0,0 +1,15 @@
+package converter;
+
+public class XMLConverter {
+
+	public static void main(String[] args) {
+		if(args.length < 2) {
+			throw new IllegalArgumentException("Se deben introducir al menos dos nombres");
+		}
+		String xmlName = args[0];
+		String output = args[1];
+		
+		
+	}
+
+}
